f1d7728c5ffa254ac8373e7746ed45f3e0978971
Marcoslipic <mjlipic.37@gmail.com>
2020-11-30 14:48:08 -0700

    app with working videos

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactPlayer from "react-player" 
 // import { Player } from 'video-react';
 
 export default function Songs(props) {
@@ -6,16 +7,9 @@ export default function Songs(props) {
     return (
         <div>
             {props.song.title}
-            {/* <iframe
-        title="video"
-        height="200"
-        width="300" 
-        src={props.song.video} alt={props.song.id}
-            // frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen
-            >
-            </iframe> */}
 
-            <iframe width="560" height="315" src={props.song.video} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <ReactPlayer
+                url={props.song.video}/>
 
         </div>
     )
